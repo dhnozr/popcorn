51b79d38d4ca057dfbeded958bd6624ceaa9778a
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-09-10 22:33:00 +0300

    refactor states and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar';
+import useMovies from './hooks/useMovies';
 
 export default function App() {
+  const [query, setQuery] = useState('');
+  const { movies, error, isLoading } = useMovies(query);
+
   return (
     <>
-      <Navbar />
+      <Navbar query={query} setQuery={setQuery} />
     </>
   );
 }
