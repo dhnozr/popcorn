b49047cd7c2829a2ecdd541311d712c109741f48
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-09-10 23:09:51 +0300

    new straacture for ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import useMovies from './hooks/useMovies';
+import Main from './components/Main';
+import Box from './components/Box';
+import MovieList from './components/MovieList';
+import Loader from './components/Loader';
+import ErrorMessage from './components/ErrorMessage';
 
 export default function App() {
   const [query, setQuery] = useState('');
@@ -9,6 +14,17 @@ export default function App() {
   return (
     <>
       <Navbar query={query} setQuery={setQuery} />
+      <Main>
+        <Box>
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
+        </Box>
+
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+      </Main>
     </>
   );
 }
