a5862fb48462f919fe013e2da12970ba7ee06ec5
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-09-12 16:53:07 +0300

    Types changed and states created for movies and selecting id

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,37 @@ import Box from './components/Box';
 import MovieList from './components/MovieList';
 import Loader from './components/Loader';
 import ErrorMessage from './components/ErrorMessage';
+import WatchedSummary from './components/WatchedSummary';
+import WatchedMovieList from './components/WatchedMovieList';
+import MovieDetails from './components/MovieDetails';
+
+export interface MovieList {
+  Title: string | undefined;
+  Year: string | undefined;
+  imdbID: string;
+  Poster: string | undefined;
+  Plot?: string | undefined;
+  Runtime?: string | number; // Runtime API'den string olarak gelebilir
+  Released?: number | undefined; // Released string ya da number olabilir, tipini güncelledik
+  Genre?: string | undefined;
+  imdbRating?: string | number; // imdbRating hem string hem de number olabilir
+  Actors?: string | undefined;
+  Director?: string | undefined;
+}
 
 export default function App() {
   const [query, setQuery] = useState('');
   const { movies, error, isLoading } = useMovies(query);
+  const [watched, setWatched] = useState<MovieList[]>([]);
+  const [selectedId, setSelectedId] = useState<null | string>(null);
+
+  function handleSelectMovie(id: string) {
+    setSelectedId(selectedId => (id === selectedId ? null : id));
+  }
+
+  function handleAddWatchMovie(newMovie: MovieList) {
+    setWatched(watched => [...watched, newMovie]);
+  }
 
   return (
     <>
@@ -17,12 +44,19 @@ export default function App() {
       <Main>
         <Box>
           {isLoading && <Loader />}
-          {!isLoading && !error && <MovieList movies={movies} />}
+          {!isLoading && !error && <MovieList movies={movies} onSelectMovie={handleSelectMovie} />}
           {error && <ErrorMessage message={error} />}
         </Box>
 
         <Box>
-          <MovieList movies={movies} />
+          {selectedId ? (
+            <MovieDetails onAddWatch={handleAddWatchMovie} selectedId={selectedId} watched={watched} />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList watched={watched} />
+            </>
+          )}
         </Box>
       </Main>
     </>
