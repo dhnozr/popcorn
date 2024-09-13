5bbbb856b6d6ea1ac7f8fd08edf2c554e4662e51
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-09-13 14:04:55 +0300

    watched list mapped

--- src/components/WatchedMovieList.tsx
+++ src/components/WatchedMovieList.tsx
@@ -1,9 +1,17 @@
 import { MovieList } from '../App';
+import WatchedMovie from './WatchedMovie';
 
 interface WatchedMoviesProps {
   watched: MovieList[];
+  onDeleteWatched: (id: string) => void;
 }
 
-export default function WatchedMovieList({ watched }: WatchedMoviesProps) {
-  return <div>WatchedMovieList</div>;
+export default function WatchedMovieList({ watched, onDeleteWatched }: WatchedMoviesProps) {
+  return (
+    <ul className='list'>
+      {watched.map(movie => (
+        <WatchedMovie movie={movie} key={movie.imdbID} onDeleteWatched={onDeleteWatched} />
+      ))}
+    </ul>
+  );
 }
