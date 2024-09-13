22e8f179d7dbf9da5db4e47855b6079f2749c65f
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-09-13 14:06:07 +0300

    movies length calculated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
+import { MovieList } from '../App';
+
 interface NavbarProps {
   setQuery: (query: string) => void;
   query: string;
+  movies: MovieList[];
 }
 
-export default function Navbar({ setQuery, query }: NavbarProps) {
+export default function Navbar({ setQuery, query, movies }: NavbarProps) {
   function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
     setQuery(e.target.value);
   }
@@ -16,7 +19,7 @@ export default function Navbar({ setQuery, query }: NavbarProps) {
       </div>
       <input className='search' type='text' placeholder='Search movies...' onChange={handleOnChange} value={query} />
       <p className='num-results'>
-        Found <strong>x</strong> results
+        Found <strong>{movies?.length}</strong> results
       </p>
     </nav>
   );
